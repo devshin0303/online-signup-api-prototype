268a1c9c859cd3f3129e92a48fe5db2a6ba7c649
devshin0303 <ejrdl0303@gmail.com>
2024-08-07 17:34:15 +0900

    add common module ( database, config )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, DatabaseModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
